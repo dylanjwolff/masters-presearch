d96481405d6a5c1087c0ddc60cc9d972816c0756
wolffd <wolffdy0@gmail.com>
2020-02-24 19:30:39 +0100

    added reducer

---
+++ diff-matlab-compiler/driver/reduction/gcd.m
@@ -0,0 +1,265 @@
+function [g, c, d] = gcd(a)
+
+b = 17;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if length(a) == 1
+   a = a(ones(size(b))) ;
+elseif length(b) == 1
+   b = b(ones(size(a))) ;
+end
+
+if ~isequal(size(a),size(b))
+    error('Inputs must be the same size.')
+else
+    siz = size(a) ;
+    a = a(:) ;      b = b(:) ;
+end
+
+if ~isequal(round(a),a) | ~isequal(round(b),b)
+    error('Requires integer input arguments.')
+end
+ 
+for k = 1 : length(a)            % length(a) decides the no of sets.
+    u = [ 1  0  abs(a(k)) ] ;
+    v = [ 0  1  abs(b(k)) ] ;
+    
+
+
+    
+    while v(3)    % ie, till the rem becomes 0 for EACH set
+
+    
+
+
+
+
+        q = floor ( u(3) / v(3) ) ;
+        t = u - v * q ;
+        u = v ;
+        v = t ;
+    end
+    
+
+    
+    c(k) = u(1) * sign(a(k)) ;
+    d(k) = u(2) * sign(b(k)) ;
+    g(k) = u(3) ;
+    
+
+
+
+    
+end
+
+c = reshape(c,siz) ;
+d = reshape(d,siz) ;
+g = reshape(g,siz) ;
+
+g
+c
+d
---
+++ diff-matlab-compiler/driver/reduction/reducer.hs
@@ -0,0 +1,33 @@
+#!/usr/bin/env stack
+-- stack --resolver lts-15.0 script
+{-# LANGUAGE OverloadedStrings #-}
+import Turtle
+import Data.Text
+import Data.IORef
+
+
+shelln cmd = shell cmd Turtle.empty
+
+argument = "0"
+file_path  = decodeString "gcd.m"
+(n, e) = splitExtension file_path
+
+
+main = do
+        ec <- (shelln (fromString ("mcc -m " ++ encodeString file_path)))
+        let compiled_out = inshell (fromString ("./run_" ++ encodeString n ++ ".sh /usr/local/MATLAB/MATLAB_Runtime/v97/ " ++ argument)) Turtle.empty
+            interp_out = inshell (fromString ("matlab -batch '" ++ encodeString n ++ "(" ++ argument ++ ")'")) Turtle.empty in do
+                interp_out <- fmap strip $ strict interp_out
+                compiled_out <- fmap strip $ strict $ dropn 4 compiled_out
+                print compiled_out
+                print interp_out
+                return $ if (isInfixOf compiled_out interp_out) then ExitFailure 1 else ExitSuccess
+
+dropn :: Int -> Shell a -> Shell a
+dropn n s = Shell (\(FoldShell step begin done) -> do
+    ref <- newIORef 0  -- I feel so dirty
+    let step' x a = do
+            n' <- readIORef ref
+            writeIORef ref (n' + 1)
+            if n' < n then return x else step x a
+    _foldShell s (FoldShell step' begin done) )
